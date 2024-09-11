25c4ca9862aee37586362c900cfe53a86b1f040e
0helluve <0helluve@gmail.com>
2024-09-11 12:55:57 +0530

    Delete server directory

--- server/helpers/paypal.js
+++
@@ -1,9 +0,0 @@
-const paypal = require("paypal-rest-sdk");
-
-paypal.configure({
-  mode: "sandbox",
-  client_id: "Af1LWJRF0ZmAzvImu49RjUPUP37nHwIGDohi7CjxrGDq-UNSGmbSmS9yWD7rhX2F2bdAIkKsI3q7zbCy",
-  client_secret: "EOQRGp8hT-DznWgcdMmjvBF_bMLaAyj_faFe7Y2pml4aiFWFxwDyn8xRlCB79WULdNAQ1xbzcv7LZ3xJ",
-});
-
-module.exports = paypal;
